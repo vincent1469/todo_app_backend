15ccb581198e2d75e3dc8b559b9e942cbb1ec386
Vincent Chu <vincentchu1988@gmail.com>
2018-08-19 20:03:37 +0000

    broken delete button

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,5 +1,6 @@
 class TodoController <ApplicationController
     def index
+        @todos=Todo.all
     end
     def show
         @t=Todo.find(params[:id])
@@ -13,5 +14,10 @@ class TodoController <ApplicationController
         t.completed=false
         t.save
         redirect_to"/todo/show/#{t.id}"
+    end    
+    def delete
+        @todo_delete=Todo.destroy
+        t.save
+        redirect_to"/todo/index"
     end    
 end
