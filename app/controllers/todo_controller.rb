b259c30bab6eb622330c705ff87be3ace0ba3249
Vincent Chu <vincentchu1988@gmail.com>
2018-08-19 18:40:01 +0000

    form

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,4 +4,14 @@ class TodoController <ApplicationController
     def show
         @t=Todo.find(params[:id])
     end
+    def new
+    end    
+    def create
+        t=Todo.new
+        t.description=params[:description]
+        t.pomodoro_estimate=params[:pomodoro_estimate]
+        t.completed=false
+        t.save
+        redirect_to"/todo/show/#{t.id}"
+    end    
 end
